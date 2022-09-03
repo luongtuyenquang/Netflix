5b3ef1a2781805dd526cd116b577cc1809079755
unknown <quang.luong2@executionlab.asia>
2022-09-03 18:02:46 +0700

    Build UI Banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Banner from '../components/Banner'
 
 const Home: NextPage = () => {
     return (
@@ -15,6 +16,7 @@ const Home: NextPage = () => {
             <div className='container-fluid'>
                 <div className='container'>
                     <Header />
+                    <Banner />
                 </div>
             </div>
         </div>

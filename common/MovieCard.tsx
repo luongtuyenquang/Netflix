8d03a3e5bb9de6156fd82bc7bc67ad43e6451b53
unknown <quang.luong2@executionlab.asia>
2022-09-06 11:30:56 +0700

    common: grid + using tag img

--- common/MovieCard.tsx
+++ common/MovieCard.tsx
@@ -1,17 +1,9 @@
-import Image from 'next/image'
-
 function MovieCard({ image, name, originName, slug }) {
     return (
         <a href={`/movie/${slug ? slug : ''}`} className='movie-card'>
-            {image ? (
-                <Image
-                    src={image}
-                    width='178'
-                    height='267'
-                    className='movie-card__image'
-                    alt='Image'
-                />
-            ) : null}
+            <div className='movie-card__image'>
+                {image ? <img src={image} alt='Image' /> : null}
+            </div>
             <div className='movie-card__info'>
                 <p className='movie-card__name-vn'>{name}</p>
                 <p className='movie-card__name-us'>{originName}</p>

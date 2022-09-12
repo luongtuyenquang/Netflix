19844633fdfdb125651000dbdf82eb8bc53cc7f3
unknown <quang.luong2@executionlab.asia>
2022-09-12 20:38:43 +0700

    handle routing: footer item link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../public/assets/images/logo-netflix.svg'
 import { ButtonLink } from '../common/Button'
+import footerLink from '../store-data/footerLink'
 
 const Footer: React.FC = () => {
     return (
@@ -12,38 +13,44 @@ const Footer: React.FC = () => {
                         <Image src={logo} width='90' height='30' />
                     </a>
                 </Link>
-                <p className='footer__item-text footer__item--pr-7'>
+                <p className='footer__item-text footer__item--pr-7 footer__item-text--unhover'>
                     Xem phim mới miễn phí nhanh chất lượng cao. Xem Phim online Việt Sub, Thuyết
                     minh, lồng tiếng chất lượng HD. Xem phim nhanh online chất lượng cao
                 </p>
             </div>
             <div className='footer__item'>
                 <p className='footer__item-title'>Phim mới</p>
-                <p className='footer__item-text'>Phim bộ mới</p>
-                <p className='footer__item-text'>Phim sắp chiếu</p>
-                <p className='footer__item-text'>Phim thuyết minh</p>
-                <p className='footer__item-text'>Phim chiếu rạp</p>
+                {footerLink.newMovie.map((item) => {
+                    return (
+                        <Link href={item.link}>
+                            <a className={item.className}>{item.name}</a>
+                        </Link>
+                    )
+                })}
             </div>
             <div className='footer__item'>
                 <p className='footer__item-title'>Phim lẻ</p>
-                <p className='footer__item-text'>Phim hành động</p>
-                <p className='footer__item-text'>Phim kinh dị</p>
-                <p className='footer__item-text'>Phim hoạt hình</p>
-                <p className='footer__item-text'>Phim viễn tưởng</p>
+                {footerLink.singleMovie.map((item) => {
+                    return (
+                        <Link href={item.link}>
+                            <a className={item.className}>{item.name}</a>
+                        </Link>
+                    )
+                })}
             </div>
             <div className='footer__item'>
                 <p className='footer__item-title'>Liên hệ với tôi</p>
-                <p className='footer__item-text'>
+                <p className='footer__item-text footer__item-text--unhover'>
                     Điện thoại: <span className='footer__item--bold'>032 948 6975</span>
                 </p>
-                <p className='footer__item-text'>
+                <p className='footer__item-text footer__item-text--unhover'>
                     Email: <span className='footer__item--bold'>luongtuyenquang.it@gmail.com</span>
                 </p>
-                <p className='footer__item-text'>
+                <p className='footer__item-text footer__item-text--unhover'>
                     Facebook:
                     <ButtonLink
                         href='https://www.facebook.com/fb.luongtuyenquang'
-                        className='footer__item--bold'
+                        className='footer__item--bold footer__item--fb'
                         target='_blank'
                     >
                         Lương Tuyên Quang
---
+++ store-data/footerLink.tsx
@@ -0,0 +1,48 @@
+const footerLink = {
+    newMovie: [
+        {
+            name: 'Phim bộ',
+            link: '/movie/series',
+            className: 'footer__item-text',
+        },
+        {
+            name: 'Phim sắp chiếu',
+            link: '/404',
+            className: 'footer__item-text',
+        },
+        {
+            name: 'Phim thuyết minh',
+            link: '/404',
+            className: 'footer__item-text',
+        },
+        {
+            name: 'Phim chiếu rạp',
+            link: '/404',
+            className: 'footer__item-text',
+        },
+    ],
+    singleMovie: [
+        {
+            name: 'Phim hành động',
+            link: '/404',
+            className: 'footer__item-text',
+        },
+        {
+            name: 'Phim kinh dị',
+            link: '/404',
+            className: 'footer__item-text',
+        },
+        {
+            name: 'Phim hoạt hình',
+            link: '/404',
+            className: 'footer__item-text',
+        },
+        {
+            name: 'Phim viễn tưởng',
+            link: '/404',
+            className: 'footer__item-text',
+        },
+    ],
+}
+
+export default footerLink
